7458d936820059144966913e11a3100dfa262649
Mikhiel <mikhielbenji9@gmail.com>
2024-06-20 23:01:16 +0530

    try-catch

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom";
 import { RouterProvider } from "react-router-dom";
-import {router} from "./router.tsx"
-import './index.css'
+import { router } from "./router";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import "./index.css";
+
+ReactDOM.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,19 @@
-import { Route } from "react-router-dom";
-import { createRoutesFromElements } from "react-router-dom";
-import { createBrowserRouter } from "react-router-dom";
-
+import {
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+} from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import Form from "./pages/Form";
 import Result from "./pages/Result";
 
-
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="" element={<Home />} />
-      <Route path="/form" element={<Form />} />
-      <Route path="/result" element={<Result />} />
+      <Route index element={<Home />} /> {/* Changed path="" to index */}
+      <Route path="form" element={<Form />} />
+      <Route path="result" element={<Result />} />
     </Route>
   )
 );
